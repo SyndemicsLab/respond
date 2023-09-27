99b0df3283b885eca1eb5eaf11fbc9d47d992a43
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2023-09-27 13:53:41 -0400

    Re-enable tests in `build.sh`. Add a conan build script for convenience.

---
+++ conan-build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# create the build directory if it doesn't exist, otherwise ensure it's empty
+([[ -d build ]] && rm -rf build/*) || mkdir build
+# create a conan profile based on current environment
+conan profile detect --force
+# install missing dependencies
+conan install . --output-folder=build --build=missing -s build_type=Debug -s compiler.cppstd=gnu14
+# enter a subshell and build the model
+(
+   cd build || exit
+   cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+   cmake --build . --config Debug
+   # run code tests if they built
+   [[ -f test/respondTest ]] && test/respondTest
+)
