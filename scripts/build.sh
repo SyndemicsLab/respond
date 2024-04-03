6451470b471e980d82ff1387a8f9f77c29aba7ec
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-04-03 12:14:53 -0400

    Fix `build.sh` error(s).

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 # only execute these lines if the `module` command is present in the environment
 if command -v module &>/dev/null; then
     module load python3/3.10.12
@@ -9,7 +10,7 @@ cd "$(git rev-parse --show-toplevel)"
 CONANPATH=$(command -v conan)
 
 # install conan, if not found in the current scope
-if [[ -z "$CONANPATH"]]; then
+if [[ -z "$CONANPATH" ]]; then
     echo "The \`conan\` command is not found\! Attempting a local installation..."
     if [[ -z "$VIRTUAL_ENV" ]]; then
 	if ! pip install --user conan; then
@@ -25,12 +26,12 @@ if [[ -z "$CONANPATH"]]; then
     CONANPATH="python3 -m conans.conan"
 fi
 
+# ensure the `build/` directory exists
+([[ -d "build/" ]] && rm -rf build/*) || mkdir "build/"
+
 # install dependencies via conan
 $CONANPATH profile detect --force
 $CONANPATH install . --build=missing --settings=build_type=Debug
-
-# ensure the `build/` directory exists
-([[ -d "build/" ]] && rm -rf build/*) || mkdir "build/"
 
 (
     cd "build" || exit
@@ -42,7 +43,8 @@ $CONANPATH install . --build=missing --settings=build_type=Debug
 	exit 1
     fi
     cmake .. -DCMAKE_TOOLCHAIN_FILE=Debug/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
-    # determine the number of processing units available
+    (
+	# determine the number of processing units available
 	CORES="$(nproc --all)"
 	# if CORES > 1 compile in parallel where possible
 	([[ -n "$CORES" ]] && cmake --build . -j"$CORES") || cmake --build .
