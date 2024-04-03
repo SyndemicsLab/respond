89009cffeb79aefbcae080e3aa52ec29e1cb1673
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-04-03 12:05:58 -0400

    Update `build.sh` to account for more user environment scenarios.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,52 @@
-module load python3/3.10.12
-pip install conan
-module load gcc/12.2.0
+# only execute these lines if the `module` command is present in the environment
+if command -v module &>/dev/null; then
+    module load python3/3.10.12
+    module load gcc/12.2.0
+fi
 
+# change to the top-level git folder
 cd "$(git rev-parse --show-toplevel)"
+CONANPATH=$(command -v conan)
 
-$HOME/.local/bin/conan profile detect --force
-$HOME/.local/bin/conan install . --build=missing --settings=build_type=Debug
-cd build
-source Debug/generators/conanbuild.sh
-cmake .. -DCMAKE_TOOLCHAIN_FILE=Debug/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
-cmake --build .
-source Debug/generators/deactivate_conanbuild.sh
+# install conan, if not found in the current scope
+if [[ -z "$CONANPATH"]]; then
+    echo "The \`conan\` command is not found\! Attempting a local installation..."
+    if [[ -z "$VIRTUAL_ENV" ]]; then
+	if ! pip install --user conan; then
+	    echo "\`conan\` installation failed. Exiting."
+	    exit 1
+	fi
+    else
+	if ! pip install conan; then
+	    echo "\`conan\` installation failed. Exiting."
+	    exit 1
+	fi
+    fi
+    CONANPATH="python3 -m conans.conan"
+fi
+
+# install dependencies via conan
+$CONANPATH profile detect --force
+$CONANPATH install . --build=missing --settings=build_type=Debug
+
+# ensure the `build/` directory exists
+([[ -d "build/" ]] && rm -rf build/*) || mkdir "build/"
+
+(
+    cd "build" || exit
+    # check if the conan generator file was generated successfully
+    if [[ -f "Debug/generators/conanbuild.sh" ]]; then
+	source "Debug/generators/conanbuild.sh"
+    else
+	echo "\`conan\` generator failed. Terminating."
+	exit 1
+    fi
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=Debug/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug
+    # determine the number of processing units available
+	CORES="$(nproc --all)"
+	# if CORES > 1 compile in parallel where possible
+	([[ -n "$CORES" ]] && cmake --build . -j"$CORES") || cmake --build .
+    )
+    # deactivate the conan virtual environment
+    source "Debug/generators/deactivate_conanbuild.sh"
+)
