8528e18aa8b03ca4b8a328bcef2bce8ce5244e13
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-06-11 13:33:23 -0400

    Revamping `build.sh` to be analogous to HEPCEv2 Scope 1 build script. (#72)
    * Revamping `build.sh` to be analogous to HEPCEv2 Scope 1 build script.
    * Resolving issues in `CMAKE_COMMAND` arising due to over-quoting.
    * Consistently using spaces instead of a combination of tabs and spaces.
    * Adding `CMAKE_POSITION_INDEPENDENT_CODE` option for building shared objects when compiling Python language bindings.
    * Removing `debug.sh` and `release.sh` for redundancy to the newly-consolidated `build.sh`.
    * Fix typo in "Build and Test" workflow.
    * Fix (hopefully) the last typo wrt build type

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,67 +1,128 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -l
 # only execute these lines if the `module` command is present in the environment
+# used for the BU SCC
 if command -v module &>/dev/null; then
-    module load python3/3.10.12
     module load gcc/12.2.0
+    module load miniconda
 fi
 
-if [[ -z "$1" ]]; then
-    BUILDTYPE="Debug"
-else
-    case "$1" in
-	"Debug"|"Release"|"Build")
-	    BUILDTYPE="$1"
-	    ;;
-	*)
-	    echo "Specified build type is invalid\!"
-	    ;;
+# help message to be output either with the -h flag or when using invalid syntax
+showhelp () {
+    printf "\033[31m%s\033[0m" "$0 - Build the RESPOND Model"
+    echo
+    echo
+    echo "Syntax: $(basename "$0") [-h|-t OPTION|-p|-b]"
+    echo "h              Print this help screen."
+    echo "t OPTION       Set the build type to OPTION"
+    echo "               Options: [Debug|Release|Build]"
+    echo "               Default: Debug"
+    echo "p              Build and run tests."
+    echo "b              Build Python Language Bindings"
+}
+
+# set default build type
+BUILDTYPE="Debug"
+BUILD_TESTS=""
+BUILD_PYBINDINGS=""
+
+# process optional command line flags
+while getopts ":bhpt:" option; do
+    case $option in
+        h)
+            showhelp
+            exit
+            ;;
+        t)
+            case "$OPTARG" in
+                "Debug"|"Release")
+                    BUILDTYPE="$OPTARG"
+                    ;;
+                *)
+                    echo "Specified build type is invalid!"
+                    exit
+                    ;;
+            esac
+            ;;
+        p)
+            BUILD_TESTS="ON"
+            ;;
+        b)
+            BUILD_PYBINDINGS="ON"
+            ;;
+        \?)
+            echo "Error: Invalid option flag provided!"
+            showhelp
+            exit
+            ;;
     esac
-fi
-
-# change to the top-level git folder
-cd "$(git rev-parse --show-toplevel)"
-CONANPATH=$(command -v conan)
-
-# install conan, if not found in the current scope
-if [[ -z "$CONANPATH" ]]; then
-    echo "The \`conan\` command is not found\! Attempting a local installation..."
-    if [[ -z "$VIRTUAL_ENV" ]]; then
-	if ! pip install --user conan; then
-	    echo "\`conan\` installation failed. Exiting."
-	    exit 1
-	fi
-    else
-	if ! pip install conan; then
-	    echo "\`conan\` installation failed. Exiting."
-	    exit 1
-	fi
-    fi
-    CONANPATH="python3 -m conans.conan"
-fi
-
-# ensure the `build/` directory exists
-([[ -d "build/" ]] && rm -rf build/*) || mkdir "build/"
-
-# install dependencies via conan
-$CONANPATH profile detect --force
-$CONANPATH install . --build=missing --settings=build_type="$BUILDTYPE"
+done
 
 (
-    cd "build" || exit
-    # check if the conan generator file was generated successfully
-    if [[ -f "$BUILDTYPE/generators/conanbuild.sh" ]]; then
-	source "$BUILDTYPE/generators/conanbuild.sh"
-    else
-	echo "\`conan\` generator failed. Terminating."
-	exit 1
+    # change to the top-level git folder
+    cd "$(git rev-parse --show-toplevel)" || exit
+    CONANPATH=$(command -v conan)
+
+    # install conan, if not found in the current scope
+    if [[ -z "$CONANPATH" ]]; then
+        echo "The \`conan\` command is not found\! Attempting a local installation..."
+        if [[ -z "$VIRTUAL_ENV" ]]; then
+            if ! pip install --user conan; then
+                echo "\`conan\` installation failed. Exiting."
+                exit 1
+            fi
+        else
+            if ! pip install conan; then
+                echo "\`conan\` installation failed. Exiting."
+                exit 1
+            fi
+        fi
+        CONANPATH="python3 -m conans.conan"
     fi
-    cmake .. -DCMAKE_TOOLCHAIN_FILE="$BUILDTYPE"/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE="$BUILDTYPE"
+
+    # ensure the `build/` directory exists
+    ([[ -d "build/" ]] && rm -rf build/*) || mkdir "build/"
+
+    # install dependencies via conan
+    if [[ ! -f "$HOME/.conan2/profiles/default" ]]; then
+        $CONANPATH profile detect --force
+    fi
+
+    $CONANPATH install . --build=missing --settings=build_type="$BUILDTYPE"
+
     (
-	# determine the number of processing units available
-	CORES="$(nproc --all)"
-	# if CORES > 1 compile in parallel where possible
-	([[ -n "$CORES" ]] && cmake --build . -j"$CORES") || cmake --build .
+        cd "build" || exit
+        # check if the conan generator file was generated successfully
+        if [[ -f "$BUILDTYPE/generators/conanbuild.sh" ]]; then
+            # shellcheck source=/dev/null
+            source "$BUILDTYPE/generators/conanbuild.sh"
+        else
+            echo "\`conan\` generator failed. Terminating."
+            exit 1
+        fi
+
+        # build tests, if specified
+        CMAKE_COMMAND="cmake .. -DCMAKE_TOOLCHAIN_FILE=$BUILDTYPE/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE"
+        if [[ -n "$BUILD_TESTS" ]]; then
+	    CMAKE_COMMAND="$CMAKE_COMMAND -DBUILD_TESTS=$BUILD_TESTS"
+	fi
+	# build Python language bindings, if specified
+	if [[ -n "$BUILD_PYBINDINGS" ]]; then
+	    CMAKE_COMMAND="$CMAKE_COMMAND -DBUILD_PYBINDINGS=$BUILD_PYBINDINGS -DCMAKE_POSITION_INDEPENDENT_CODE=ON"
+	fi
+	# run the full build command as specified
+	$CMAKE_COMMAND
+	(
+	    # determine the number of processing units available
+	    CORES="$(nproc --all)"
+	    # if CORES > 1 compile in parallel where possible
+	    ([[ -n "$CORES" ]] && cmake --build . -j"$CORES") || cmake --build .
+	)
+	# deactivate the conan virtual environment
+	# shellcheck source=/dev/null
+	source "$BUILDTYPE/generators/deactivate_conanbuild.sh"
+	# run tests, if they built properly
     )
-    # deactivate the conan virtual environment
-    source "$BUILDTYPE/generators/deactivate_conanbuild.sh"
+    if [[ (-n "$BUILD_TESTS") && (-f "bin/respondTest") ]]; then
+	bin/respondTest
+    fi
 )
