580f14af547c0e45d912f4f589465c0f5e0dfc80
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2024-06-11 11:20:37 -0400

    Consistently using spaces instead of a combination of tabs and spaces.

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,32 +28,32 @@ BUILD_PYBINDINGS=""
 # process optional command line flags
 while getopts ":bhpt:" option; do
     case $option in
-	h)
-	    showhelp
-	    exit
-	    ;;
-	t)
-	    case "$OPTARG" in
-		"Debug"|"Release")
-		    BUILDTYPE="$OPTARG"
-		    ;;
-		*)
-		    echo "Specified build type is invalid!"
-		    exit
-		    ;;
-	    esac
-	    ;;
-	p)
-	    BUILD_TESTS="ON"
-	    ;;
-	b)
-	    BUILD_PYBINDINGS="ON"
-	    ;;
-	\?)
-	    echo "Error: Invalid option flag provided!"
-	    showhelp
-	    exit
-	    ;;
+        h)
+            showhelp
+            exit
+            ;;
+        t)
+            case "$OPTARG" in
+                "Debug"|"Release")
+                    BUILDTYPE="$OPTARG"
+                    ;;
+                *)
+                    echo "Specified build type is invalid!"
+                    exit
+                    ;;
+            esac
+            ;;
+        p)
+            BUILD_TESTS="ON"
+            ;;
+        b)
+            BUILD_PYBINDINGS="ON"
+            ;;
+        \?)
+            echo "Error: Invalid option flag provided!"
+            showhelp
+            exit
+            ;;
     esac
 done
 
@@ -64,19 +64,19 @@ done
 
     # install conan, if not found in the current scope
     if [[ -z "$CONANPATH" ]]; then
-	echo "The \`conan\` command is not found\! Attempting a local installation..."
-	if [[ -z "$VIRTUAL_ENV" ]]; then
-	    if ! pip install --user conan; then
-		echo "\`conan\` installation failed. Exiting."
-		exit 1
-	    fi
-	else
-	    if ! pip install conan; then
-		echo "\`conan\` installation failed. Exiting."
-		exit 1
-	    fi
-	fi
-	CONANPATH="python3 -m conans.conan"
+        echo "The \`conan\` command is not found\! Attempting a local installation..."
+        if [[ -z "$VIRTUAL_ENV" ]]; then
+            if ! pip install --user conan; then
+                echo "\`conan\` installation failed. Exiting."
+                exit 1
+            fi
+        else
+            if ! pip install conan; then
+                echo "\`conan\` installation failed. Exiting."
+                exit 1
+            fi
+        fi
+        CONANPATH="python3 -m conans.conan"
     fi
 
     # ensure the `build/` directory exists
@@ -84,25 +84,25 @@ done
 
     # install dependencies via conan
     if [[ ! -f "$HOME/.conan2/profiles/default" ]]; then
-	$CONANPATH profile detect --force
+        $CONANPATH profile detect --force
     fi
 
     $CONANPATH install . --build=missing --settings=build_type="$BUILDTYPE"
 
     (
-	cd "build" || exit
-	# check if the conan generator file was generated successfully
-	if [[ -f "$BUILDTYPE/generators/conanbuild.sh" ]]; then
-	    # shellcheck source=/dev/null
-	    source "$BUILDTYPE/generators/conanbuild.sh"
-	else
-	    echo "\`conan\` generator failed. Terminating."
-	    exit 1
-	fi
+        cd "build" || exit
+        # check if the conan generator file was generated successfully
+        if [[ -f "$BUILDTYPE/generators/conanbuild.sh" ]]; then
+            # shellcheck source=/dev/null
+            source "$BUILDTYPE/generators/conanbuild.sh"
+        else
+            echo "\`conan\` generator failed. Terminating."
+            exit 1
+        fi
 
-	# build tests, if specified
-	CMAKE_COMMAND="cmake .. -DCMAKE_TOOLCHAIN_FILE=$BUILDTYPE/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE"
-	if [[ -n "$BUILD_TESTS" ]]; then
+        # build tests, if specified
+        CMAKE_COMMAND="cmake .. -DCMAKE_TOOLCHAIN_FILE=$BUILDTYPE/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE"
+        if [[ -n "$BUILD_TESTS" ]]; then
 	    CMAKE_COMMAND="$CMAKE_COMMAND -DBUILD_TESTS=$BUILD_TESTS"
 	fi
 	# build Python language bindings, if specified
