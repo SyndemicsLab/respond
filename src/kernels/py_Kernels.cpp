e69966166ef24e242a937cb4169529b918feed42
Matthew Carroll <matthew.j.carroll@outlook.com>
2024-12-05 15:49:15 -0500

    Updating python bindings

--- src/kernels/py_Kernels.cpp
+++ src/kernels/py_Kernels.cpp
@@ -1,5 +1,24 @@
+#include "StateTransitionModelImpl.hpp"
+#include "kernels/StateTransitionModel.hpp"
+
 #include <pybind11/pybind11.h>
 
 namespace py = pybind11;
+using namespace synmodels::kernels;
 
-void init_kernels(py::module &m) {}
+void init_kernels(py::module &m) {
+    py::class_<StateTransitionModel, StateTransitionModelImpl>(
+        m, "StateTransitionModel")
+        .def(py::init<>())
+        .def("SetState", &StateTransitionModel::SetState)
+        .def("GetState", &StateTransitionModel::GetState)
+        .def("SetTransitions", &StateTransitionModel::SetTransitions)
+        .def("GetTransitions", &StateTransitionModel::GetTransitions)
+        .def("Transition", &StateTransitionModel::Transition)
+        .def("AddState", &StateTransitionModel::AddState)
+        .def("SubtractState", &StateTransitionModel::SubtractState)
+        .def("MultiplyState", &StateTransitionModel::MultiplyState)
+        .def("ScalarMultiplyState", &StateTransitionModel::ScalarMultiplyState)
+        .def("DivideState", &StateTransitionModel::DivideState)
+        .def_static("Create", &StateTransitionModel::Create);
+}
