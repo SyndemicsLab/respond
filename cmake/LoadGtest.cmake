39b69357d876c06e85baffc632618f5714c72511
Matthew Carroll <matthew.j.carroll@outlook.com>
2025-03-24 12:46:46 -0400

    Updating tests cmake and adding test to workflow

---
+++ cmake/LoadGtest.cmake
@@ -0,0 +1,12 @@
+message(CHECK_START "Fetching spdlog...")
+list(APPEND CMAKE_MESSAGE_INDENT "  ")
+
+FetchContent_Declare(
+  googletest
+  GIT_REPOSITORY https://github.com/google/googletest.git
+  GIT_TAG 2ae29b52fdff88c52fef655fa0d245fc514ca35b
+  DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+  FIND_PACKAGE_ARGS NAMES GTest
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
