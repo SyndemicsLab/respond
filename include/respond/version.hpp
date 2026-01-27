4417602a1fa87cdf52fe694d6c5ea8f321386188
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-01-27 10:35:33 -0500

    bumping version (#126)

--- include/respond/version.hpp
+++ include/respond/version.hpp
@@ -4,10 +4,10 @@
 // Created Date: 2025-03-06                                                   //
 // Author: Matthew Carroll                                                    //
 // -----                                                                      //
-// Last Modified: 2025-12-23                                                  //
+// Last Modified: 2026-01-27                                                  //
 // Modified By: Matthew Carroll                                               //
 // -----                                                                      //
-// Copyright (c) 2025 Syndemics Lab at Boston Medical Center                  //
+// Copyright (c) 2025-2026 Syndemics Lab at Boston Medical Center             //
 ////////////////////////////////////////////////////////////////////////////////
 
 #ifndef RESPOND_VERSION_HPP_
@@ -15,7 +15,7 @@
 
 #define RESPOND_VER_MAJOR 2
 #define RESPOND_VER_MINOR 2
-#define RESPOND_VER_PATCH 0
+#define RESPOND_VER_PATCH 1
 
 #define RESPOND_TO_VERSION(major, minor, patch)                                \
     (major * 10000 + minor * 100 + patch)
