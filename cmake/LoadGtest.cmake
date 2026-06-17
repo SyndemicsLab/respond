ea36dd588ab6d6c6d3eee20334863f071cd9794f
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-17 14:00:25 -0400

    fixing windows tests, updating presets, and bumping gtest version

--- cmake/LoadGtest.cmake
+++ cmake/LoadGtest.cmake
@@ -4,7 +4,7 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG 2ae29b52fdff88c52fef655fa0d245fc514ca35b
+  GIT_TAG 52eb8108c5bdec04579160ae17225d66034bd723
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
   FIND_PACKAGE_ARGS NAMES GTest
 )
