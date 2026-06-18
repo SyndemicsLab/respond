aa42aafab7337c911c11c7d8b17be20828e23015
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-18 11:58:41 -0400

    adding coverage upload and gtest fix

--- cmake/LoadGtest.cmake
+++ cmake/LoadGtest.cmake
@@ -6,7 +6,6 @@ FetchContent_Declare(
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG 52eb8108c5bdec04579160ae17225d66034bd723
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  FIND_PACKAGE_ARGS NAMES GTest
 )
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
