f2629fdb918235a71472f0dd4e8cbf9d2c7a50c7
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-22 10:38:58 -0400

    [Hotfix] Windows Building (#135)
    * fixing windows tests, updating presets, and bumping gtest version
    
    * adding coverage upload and gtest fix
    
    * unit test hotfix
    
    * Changing gcovr to do both xml and html
    
    * gcovr filter

--- cmake/LoadGtest.cmake
+++ cmake/LoadGtest.cmake
@@ -4,9 +4,8 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG 2ae29b52fdff88c52fef655fa0d245fc514ca35b
+  GIT_TAG 52eb8108c5bdec04579160ae17225d66034bd723
   DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-  FIND_PACKAGE_ARGS NAMES GTest
 )
 # For Windows: Prevent overriding the parent project's compiler/linker settings
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
