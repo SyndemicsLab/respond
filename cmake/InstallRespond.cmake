71ccdffafa1e412fbabd706666507e9bac243511
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-24 16:53:37 -0400

    [Feature] Cleaning up CPack (#138)
    * Trying to fix installing
    
    * fixing Eigen clashes if already found
    
    * installing fixes
    
    * updating cpack
    
    * Adding nsis to release.yml and testing details
    
    * moving Eigen back to build requirement
    
    * install nsis
    
    * turning off windows
    
    * Update cmake/respondCPackOptions.cmake.in
    
    Co-authored-by: Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
    Signed-off-by: Matthew Carroll <28577806+MJC598@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Matthew Carroll <28577806+MJC598@users.noreply.github.com>
    Co-authored-by: Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>

--- cmake/InstallRespond.cmake
+++ cmake/InstallRespond.cmake
@@ -1,49 +1,63 @@
-message(STATUS "Installing respond version ${RESPOND_VERSION}...")
+message(STATUS "Installing respond version ${RESPOND_VERSION} to ${CMAKE_INSTALL_FULL_LIBDIR} and ${CMAKE_INSTALL_FULL_INCLUDEDIR}...")
 
-set(export_dest_dir "${CMAKE_INSTALL_LIBDIR}/respond")
-set(export_cmake_dir "${CMAKE_INSTALL_LIBDIR}/cmake/respond")
-set(version_config_file "${CMAKE_CURRENT_BINARY_DIR}/respondConfigVersion.cmake")
-
-#-------------------------------------------------------------------------------
-# Include files
-#-------------------------------------------------------------------------------
-message("Installing respond to ${CMAKE_INSTALL_FULL_LIBDIR} and ${CMAKE_INSTALL_FULL_INCLUDEDIR}...")
-
-install(
-    TARGETS respond_model
+# This defines where things get installed. The default is to install to the
+# system level directories (e.g. /usr/lib, /usr/respond/include). We will want
+# to change this to allow for user level install too via the
+# CMAKE_INSTALL_PREFIX variable.
+install(TARGETS respond_model
     EXPORT respondTargets
-    FILE_SET HEADERS
-)
-
-install(
-    EXPORT respondTargets
-    DESTINATION ${export_cmake_dir}
-    NAMESPACE respond::
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib
+    RUNTIME DESTINATION bin
+    INCLUDES DESTINATION include
+    FILE_SET HEADERS DESTINATION include
 )
 
 include(CMakePackageConfigHelpers)
 
+# Setup the Version config file
+write_basic_package_version_file(
+  "${CMAKE_CURRENT_BINARY_DIR}/respond/respondConfigVersion.cmake"
+  VERSION ${RESPOND_VERSION} # Variable set in root CMakeLists.txt
+  COMPATIBILITY AnyNewerVersion
+)
+
+# export the targets to a file and add the prefix. This file is used by the
+# config file to find the compiled targets during `find_package(respond)` calls.
+export(EXPORT respondTargets
+  FILE "${CMAKE_CURRENT_BINARY_DIR}/respond/respondTargets.cmake"
+  NAMESPACE respond::
+)
+
+# Creates the config file necessary for CMake to use `find_package(respond)` # after being installed.
 configure_file(
     "${PROJECT_SOURCE_DIR}/cmake/respondConfig.cmake.in"
-    "${PROJECT_BINARY_DIR}/respondConfig.cmake"
+    "${CMAKE_CURRENT_BINARY_DIR}/respond/respondConfig.cmake"
     @ONLY
 )
-set(project_config "${PROJECT_BINARY_DIR}/respondConfig.cmake")
 
-write_basic_package_version_file(
-  ${version_config_file}
-  COMPATIBILITY ExactVersion
+# Define where to install the config files and install the targets before the 
+# config files. This is so that the config files can actually find the targets
+set(ConfigPackageLocation lib/cmake/respond)
+install(
+    EXPORT respondTargets
+    FILE respondTargets.cmake
+    NAMESPACE respond::
+    DESTINATION ${ConfigPackageLocation}
 )
 
+# Finally, install the config files
 install(
     FILES
-        ${project_config}
-        ${version_config_file}
-    DESTINATION ${export_cmake_dir}
+        "${CMAKE_CURRENT_BINARY_DIR}/respond/respondConfig.cmake"
+        "${CMAKE_CURRENT_BINARY_DIR}/respond/respondConfigVersion.cmake"
+    DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/respond"
 )
 
 #-------------------------------------------------------------------------------
-# CPack Options for RESPOND
+# CPack: This helps us create various installers. All options are controlled in
+# the `respondCPackOptions.cmake.in` config file. Please do not change CPack
+# itself, only the Options file!
 #-------------------------------------------------------------------------------
 configure_file(
     "${PROJECT_SOURCE_DIR}/cmake/respondCPackOptions.cmake.in"
@@ -52,7 +66,4 @@ configure_file(
 )
 set(CPACK_PROJECT_CONFIG_FILE "${PROJECT_BINARY_DIR}/respondCPackOptions.cmake")
 
-#-------------------------------------------------------------------------------
-# Support creation of installable packages
-#-------------------------------------------------------------------------------
 include(CPack)
