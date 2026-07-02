c59a971a6be18f60f2409a4fa07cfac2834699e9
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-07-02 10:10:16 -0400

    uml and some basic constructors
    Updating public diagram
    
    updating UML
    
    reverting index and uml

--- include/respond/model.hpp
+++ include/respond/model.hpp
@@ -4,7 +4,7 @@
 // Created Date: 2026-02-05                                                   //
 // Author: Matthew Carroll                                                    //
 // -----                                                                      //
-// Last Modified: 2026-07-02                                                  //
+// Last Modified: 2026-07-06                                                  //
 // Modified By: Matthew Carroll                                               //
 // -----                                                                      //
 // Copyright (c) 2026 Syndemics Lab at Boston Medical Center                  //
