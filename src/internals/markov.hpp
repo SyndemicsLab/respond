8246c951855692565e1cdd3b6dc4bc7b60f2c6b4
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-07-08 15:02:23 -0400

    simulation test update

--- src/internals/markov.hpp
+++ src/internals/markov.hpp
@@ -4,7 +4,7 @@
 // Created Date: 2026-02-05                                                   //
 // Author: Matthew Carroll                                                    //
 // -----                                                                      //
-// Last Modified: 2026-07-09                                                  //
+// Last Modified: 2026-07-13                                                  //
 // Modified By: Matthew Carroll                                               //
 // -----                                                                      //
 // Copyright (c) 2026 Syndemics Lab at Boston Medical Center                  //
